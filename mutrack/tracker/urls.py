36e83e18b7149a20cd85ebfdafa3206380351440
Sierra Gegenheimer <sierrageg.gmail.com>
2017-12-02 18:48:32 -0800

    Add generic detail view for albums

--- mutrack/tracker/urls.py
+++ mutrack/tracker/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name = 'tracker'
 urlpatterns = [
-    url('^$', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
+    url(r'^album/(?P<artist>[^/\s]+)/(?P<album_name>[^/\s]+)', views.AlbumView.as_view(), name='album')
 ]
--- mutrack/tracker/views.py
+++ mutrack/tracker/views.py
@@ -1,7 +1,30 @@
+
+from urllib.parse import unquote_plus
+
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import generic
 
-# Create your views here.
+from .models import Album
 
+# Views
 def index(request):
     return render(request, 'tracker/index.html')
+
+
+class AlbumView(generic.DetailView):
+    model = Album
+
+    def get_object(self):
+        """Get the object we're looking for.
+
+        Uses the album_name named group in the URL. Redefined so I don't have
+        to use the (meaningless) primary key for the album in the URL.
+        """
+        artist_name = unquote_plus(self.kwargs['artist'])
+        album_name = unquote_plus(self.kwargs['album_name'])
+        super_qset = super(AlbumView, self).get_queryset()
+
+        filterset = super_qset.filter(artist__name__iexact=artist_name,
+                                      name__iexact=album_name)
+
+        return filterset[0]
