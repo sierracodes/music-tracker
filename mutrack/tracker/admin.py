a75eacbb08002ae2f338cb2fef94604a94346f4f
Sierra Gegenheimer <sierrageg.gmail.com>
2017-12-03 13:21:01 -0800

    Add genre model and primary/secondary genre fields for album model

--- mutrack/tracker/admin.py
+++ mutrack/tracker/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from tracker.models import Artist, Album
+from tracker.models import Artist, Album, PrimaryGenre
 
 # Register your models here.
 admin.site.register(Artist)
 admin.site.register(Album)
+admin.site.register(PrimaryGenre)
--- mutrack/tracker/models.py
+++ mutrack/tracker/models.py
@@ -22,6 +22,16 @@ def validate_zero_to_five(value):
     if value < 0.0 or value > 5.0:
         raise ValidationError(_('Value not between 0 and 5.'))
 
+class PrimaryGenre(models.Model):
+    """A model representing a particular genre of music.
+
+    These genres will be deliberately broad, so that there are only a handful.
+    """
+    name = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.name
+
 
 class Artist(models.Model):
     """Model representing an artist.
@@ -39,6 +49,9 @@ class Album(models.Model):
     year = models.IntegerField()
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
     rating = models.FloatField(validators=[validate_zero_to_five])
+    primary_genre = models.ForeignKey(PrimaryGenre, on_delete=models.CASCADE)
+    secondary_genres = models.CharField(max_length=200, blank=True, default='')
+    comments = models.TextField(blank=True, default='')
 
     def __str__(self):
         return '[Album: {}]'.format(self.name)
