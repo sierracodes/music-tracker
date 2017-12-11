201c4336510b49b4705695efb1bdd514ed4da241
Sierra Gegenheimer <sierrageg.gmail.com>
2017-12-10 16:45:50 -0800

    Replace combo box + one search box with search boxes in each column

--- mutrack/tracker/static/tracker/script.js
+++ mutrack/tracker/static/tracker/script.js
@@ -8,34 +8,45 @@ jQuery.expr[':'].containsNoCase = function(a, i, m) {
       .indexOf(m[3].toUpperCase()) >= 0;
 };
 
-
 // Attach event handlers for search inputs
-$("#album-search").keyup(filterAlbums);
+$("#artist-search").keyup(filterArtist);
+$("#album-search").keyup(filterAlbum);
+$("#year-search").keyup(filterYear);
+$("#rating-search").keyup(filterRating);
+$("#genre-search").keyup(filterGenre);
 
-function filterAlbums(event) {
+function filterArtist(event) {
   var text = event.target.value;
-  var selectingOn = $("#search-selector").val();
-
-  // Get matching and non-matching rows
-  if (selectingOn === "artist") {
-    var match = $(".artist-cell:containsNoCase('text')".replace('text', text));
-    var nomatch = $(".artist-cell:not(.artist-cell:containsNoCase('text'))".replace('text', text));
-  }
-  else if (selectingOn === "album") {
-    var match = $(".album-cell:containsNoCase('text')".replace('text', text));
-    var nomatch = $(".album-cell:not(.album-cell:containsNoCase('text'))".replace('text', text));
-  }
-  else if (selectingOn === "year") {
-    var match = $(".year-cell:containsNoCase('text')".replace('text', text));
-    var nomatch = $(".year-cell:not(.year-cell:containsNoCase('text'))".replace('text', text));
-  }
-  else if (selectingOn === "genres") {
-    var match = $(".genre-cell:containsNoCase('text')".replace('text', text));
-    var nomatch = $(".genre-cell:not(.genre-cell:containsNoCase('text'))".replace('text', text));
-  }
-  // Show/hide rows according to filter
+  var match = $(".artist-cell:containsNoCase('text')".replace('text', text));
+  var nomatch = $(".artist-cell:not(.artist-cell:containsNoCase('text'))".replace('text', text));
   match.parents('tr').show();
   nomatch.parents('tr').hide();
-
-
+}
+function filterAlbum(event) {
+  var text = event.target.value;
+  var match = $(".album-cell:containsNoCase('text')".replace('text', text));
+  var nomatch = $(".album-cell:not(.album-cell:containsNoCase('text'))".replace('text', text));
+  match.parents('tr').show();
+  nomatch.parents('tr').hide();
+}
+function filterYear(event) {
+  var text = event.target.value;
+  var match = $(".year-cell:containsNoCase('text')".replace('text', text));
+  var nomatch = $(".year-cell:not(.year-cell:containsNoCase('text'))".replace('text', text));
+  match.parents('tr').show();
+  nomatch.parents('tr').hide();
+}
+function filterRating(event) {
+  var text = event.target.value;
+  var match = $(".rating-cell:containsNoCase('text')".replace('text', text));
+  var nomatch = $(".rating-cell:not(.genre-cell:containsNoCase('text'))".replace('text', text));
+  match.parents('tr').show();
+  nomatch.parents('tr').hide();
+}
+function filterGenre(event) {
+  var text = event.target.value;
+  var match = $(".genre-cell:containsNoCase('text')".replace('text', text));
+  var nomatch = $(".genre-cell:not(.genre-cell:containsNoCase('text'))".replace('text', text));
+  match.parents('tr').show();
+  nomatch.parents('tr').hide();
 }
