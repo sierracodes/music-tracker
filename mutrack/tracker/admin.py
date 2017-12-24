880116b05c3cda1d3b5bb35be06ccdee99da0c16
Sierra Gegenheimer <sierrageg.gmail.com>
2017-12-23 21:15:06 -0800

    Add Listen to admin site

--- mutrack/tracker/admin.py
+++ mutrack/tracker/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
-from tracker.models import Artist, Album, PrimaryGenre
+from tracker.models import Artist, Album, PrimaryGenre, Listen
 
 # Register your models here.
 admin.site.register(Artist)
 admin.site.register(PrimaryGenre)
+admin.site.register(Listen)
 
 class AlbumAdmin(admin.ModelAdmin):
     list_display = ['name', 'artist_name', 'year', 'rating']
