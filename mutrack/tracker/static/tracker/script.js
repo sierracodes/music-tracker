2d76aa194d87f1795586de3fc338506b22712f4d
Sierra Gegenheimer <sierrageg.gmail.com>
2017-12-30 16:35:29 -0800

    Fix search filters so they work together

--- mutrack/tracker/static/tracker/script.js
+++ mutrack/tracker/static/tracker/script.js
@@ -9,52 +9,51 @@ jQuery.expr[':'].containsNoCase = function(a, i, m) {
 };
 
 // Attach event handlers for search inputs
-$("#artist-search").keyup(filterArtist);
-$("#album-search").keyup(filterAlbum);
-$("#year-search").keyup(filterYear);
-$("#rating-search").keyup(filterRating);
-$("#genre-search").keyup(filterGenre);
-$("#play-search").keyup(filterPlays);
+$('#artist-search').keyup(filterRows);
+$('#album-search').keyup(filterRows);
+$('#year-search').keyup(filterRows);
+$('#rating-search').keyup(filterRows);
+$('#genre-search').keyup(filterRows);
+$('#plays-search').keyup(filterRows);
 
-function filterArtist(event) {
-  var text = event.target.value;
-  var match = $(".artist-cell:containsNoCase('text')".replace('text', text));
-  var nomatch = $(".artist-cell:not(.artist-cell:containsNoCase('text'))".replace('text', text));
-  match.parents('tr').show();
-  nomatch.parents('tr').hide();
+// Filter rows based on search inputs
+function filterRows(event) {
+
+  // Start by marking all rows as not filtered out
+  var allRows = $('.album-data-row');
+  for (i = 0; i < allRows.length; i++) {
+    let row = allRows[i];
+    row.setAttribute('filtered-out', 'false');
+  }
+
+  // Mark rows as filtered out according to the search inputs
+  markRowsForFilter('artist-cell', $('#artist-search').val());
+  markRowsForFilter('album-cell', $('#album-search').val());
+  markRowsForFilter('year-cell', $('#year-search').val());
+  markRowsForFilter('rating-cell', $('#rating-search').val());
+  markRowsForFilter('genre-cell', $('#genre-search').val());
+  markRowsForFilter('plays-cell', $('#plays-search').val());
+
+  // Hide and show rows appropriately
+  $('tr[filtered-out="false"]').show();
+  $('tr[filtered-out="true"]').hide();
 }
-function filterAlbum(event) {
-  var text = event.target.value;
-  var match = $(".album-cell:containsNoCase('text')".replace('text', text));
-  var nomatch = $(".album-cell:not(.album-cell:containsNoCase('text'))".replace('text', text));
-  match.parents('tr').show();
-  nomatch.parents('tr').hide();
-}
-function filterYear(event) {
-  var text = event.target.value;
-  var match = $(".year-cell:containsNoCase('text')".replace('text', text));
-  var nomatch = $(".year-cell:not(.year-cell:containsNoCase('text'))".replace('text', text));
-  match.parents('tr').show();
-  nomatch.parents('tr').hide();
-}
-function filterRating(event) {
-  var text = event.target.value;
-  var match = $(".rating-cell:containsNoCase('text')".replace('text', text));
-  var nomatch = $(".rating-cell:not(.genre-cell:containsNoCase('text'))".replace('text', text));
-  match.parents('tr').show();
-  nomatch.parents('tr').hide();
-}
-function filterGenre(event) {
-  var text = event.target.value;
-  var match = $(".genre-cell:containsNoCase('text')".replace('text', text));
-  var nomatch = $(".genre-cell:not(.genre-cell:containsNoCase('text'))".replace('text', text));
-  match.parents('tr').show();
-  nomatch.parents('tr').hide();
-}
-function filterPlays(event) {
-  var text = event.target.value;
-  var match = $(".plays-cell:containsNoCase('text')".replace('text', text));
-  var nomatch = $(".plays-cell:not(.plays-cell:containsNoCase('text'))".replace('text', text));
-  match.parents('tr').show();
-  nomatch.parents('tr').hide();
+
+// Mark all table cells of a given class which do not contain a given text
+// string (not case sensitive)
+function markRowsForFilter(cellClass, text) {
+
+  // Find cells with given class that don't contain the text
+  let selector = ".CELLCLASS:not(.CELLCLASS:containsNoCase('TEXT'))"
+  selector = selector.replace('CELLCLASS', cellClass);
+  selector = selector.replace('CELLCLASS', cellClass);
+  selector = selector.replace('TEXT', text);
+  var nomatch = $(selector);
+
+  // Mark the rows using the 'filtered-out' attribute
+  var rowsToMark = nomatch.parents('tr');
+  for (i = 0; i< rowsToMark.length; i++) {
+    let row = rowsToMark[i];
+    row.setAttribute('filtered-out', 'true');
+  }
 }
