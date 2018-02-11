23ac879d1427d926afaf2adaff6af82029f021d9
Sierra Gegenheimer <sierrageg.gmail.com>
2018-02-11 12:45:14 -0800

    Add index page for whole site

--- mutrack/mutrack/urls.py
+++ mutrack/mutrack/urls.py
@@ -17,7 +17,10 @@ from django.conf.urls import url, include
 from django.contrib.auth import views as auth_views
 from django.contrib import admin
 
+from .views import index
+
 urlpatterns = [
+    url(r'^$', index, name='index'),
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/login/?$', auth_views.LoginView.as_view(), name='login'),
     url(r'^accounts/logout/?$', auth_views.LogoutView.as_view(next_page='/tracker/'), name='logout'),
