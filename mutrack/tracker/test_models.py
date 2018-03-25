18039e036b2476f503cfb1281785089e323f5670
Sierra Gegenheimer <sierrageg.gmail.com>
2018-03-25 16:11:13 -0700

    Add test fixture for Album with a couple initial tests

--- mutrack/tracker/test_models.py
+++ mutrack/tracker/test_models.py
@@ -17,3 +17,37 @@ class ArtistTestCase(TestCase):
 
         with self.assertRaises(IntegrityError):
             Artist.objects.create(name='Queen')
+
+
+class AlbumTestCase(TestCase):
+    """Test fixture for the Album model
+    """
+
+    def test_unique_name_plus_artist(self):
+        """Test uniquness constraint of artist+album name
+        """
+        queen = Artist.objects.create(name='Queen')
+        thecure = Artist.objects.create(name='The Cure')
+
+        # Create Queen's Greatest Hits (1981 original)
+        Album.objects.create(name='Greatest Hits', year=1981, rating=5,
+                             artist=queen)
+
+        # Should not cause an error since Artist is different
+        Album.objects.create(name='Greatest Hits', year=2001, rating=4,
+                             artist=thecure)
+
+        # Verify that attempting to re-create Queen's Greatest Hits causes an
+        # error (even if it is a different year for the US re-release)
+        with self.assertRaises(IntegrityError):
+            Album.objects.create(name='Greatest Hits', year=1992, rating=5,
+                                 artist=queen)
+
+    def test_artist_name(self):
+        """Test artist_name method of Album
+        """
+        queen = Artist.objects.create(name='Queen')
+        sheer = Album.objects.create(name='Sheer Heart Attack', year=1974,
+                                     rating=5, artist=queen)
+        self.assertEqual(sheer.artist_name(), queen.name)
+        self.assertEqual(sheer.artist_name(), 'Queen')
